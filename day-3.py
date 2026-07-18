4c32fd11a5c11852744a21d706e6280ae11728a6
Hema <hemavj805@gmail.com>
2026-07-18 15:25:34 +0530

    Day-3: Variables & project- AI journey card

---
+++ day-3.py
@@ -0,0 +1,14 @@
+name= "Hema"
+age = 27
+print(name)
+print(age)
+first_name = "Hema"
+last_name = "Swaroopa"
+full_name = first_name +" "+ last_name
+print(full_name)
+birth_year = 1998
+current_year = 2026
+age = current_year - birth_year
+print(age)
+print("My name is " + full_name + " I am " + str(age) + " years old.")
+print(f"Hi, My name is {full_name} and I am {age} years old.")
---
+++ week_project1.py
@@ -0,0 +1,22 @@
+name = "Hema Swaroopa"
+City = "Hyderabad"
+current_job ="Marketing"
+Goal = "AI Engineer"
+birth_year = 1998
+current_year = 2026
+age = current_year - birth_year
+days_coding = 3
+
+print("---------------------------------------")
+print("---------------------------------------")
+print(f"         MY AI JOURNEY CARD           ")
+print("---------------------------------------")
+print("---------------------------------------")
+print(f"Name         : {name}")
+print(f"City         : {City}")
+print(f"Current job  : {current_job}")
+print(f"Goal         : {Goal}")
+print(f"Age          : {age}")
+print(f"Days Coding  : {days_coding}")
+print("---------------------------------------")
+print("---------------------------------------")
